70ee7063280434c05b203e2197b3850b845c6b04
Vatsal <koradiavatsal@gmail.com>
2016-12-24 01:25:06 +0530

    Validus

--- validus.mk
+++ validus.mk
@@ -15,12 +15,13 @@
 $(call inherit-product, device/yu/lettuce/full_lettuce.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/caf_fw.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
-PRODUCT_NAME := cm_lettuce
+PRODUCT_NAME := validus_lettuce
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := lettuce
 
